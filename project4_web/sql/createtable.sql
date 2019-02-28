d338f0d225ad1f614e64588dba2814cf6581e6de
Luke <a4010126@gmail.com>
2019-02-27 22:14:39 -0800

    modify movie title for tull text search

--- project4_web/sql/createtable.sql
+++ project4_web/sql/createtable.sql
@@ -6,7 +6,9 @@ CREATE TABLE movies (
     id VARCHAR(10) PRIMARY KEY NOT NULL,
     title VARCHAR(100) DEFAULT '',
     year INT NOT NULL,
-    director VARCHAR(100) DEFAULT ''
+    director VARCHAR(100) DEFAULT '',
+    # for full-text search.
+    FULLTEXT (title)
 );
 CREATE TABLE stars (
     id VARCHAR(100) PRIMARY KEY NOT NULL,
